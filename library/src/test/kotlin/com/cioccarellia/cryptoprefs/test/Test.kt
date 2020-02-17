d1f25ba07438e7d63add7faad7b40bea38d6f5fd
AndreaCioccarelli <andrea.cioccarelli01@gmail.com>
2020-02-17 19:41:02 +0100

    Added CircleCi & Jacoco, renamed group id

---
+++ library/src/test/kotlin/com/cioccarellia/cryptoprefs/test/Test.kt
@@ -0,0 +1,15 @@
+package com.cioccarellia.cryptoprefs.test
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class Test {
+    
+    @Test
+    fun x() {
+
+    }
+
+}
